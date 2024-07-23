92a9bf96cb382c0fe0caa12c132e65aa1a7860bb
Kirsin12 <167079449+Kirsin12@users.noreply.github.com>
2024-07-23 13:08:41 +0530

    Add files via upload

---
+++ UNION_ALL.sql
@@ -0,0 +1,50 @@
+use ddl;
+
+
+CREATE TABLE Student (
+StudentID INT PRIMARY KEY,
+StudentName VARCHAR(10)
+);
+
+INSERT INTO Student
+VALUES
+(1,'John'),
+(2,'Jack');
+
+CREATE TABLE StudentNew (
+StudentID INT PRIMARY KEY,
+StudentName VARCHAR(10)
+);
+
+INSERT INTO StudentNew
+VALUES
+(3,'John'),
+(4,'Priya'),
+(5,'Ray');
+
+CREATE TABLE Course (
+CourseID INT PRIMARY KEY,
+CourseName VARCHAR(10)
+);
+
+INSERT INTO Course
+VALUES
+(11,'Computers'),
+(12,'Physics');
+
+CREATE TABLE Enrollment (
+StudentID INT,
+CourseID INT,
+PRIMARY KEY (StudentID, CourseID),
+FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+);
+
+INSERT INTO Enrollment
+VALUES
+(1,11),
+(2,12);
+
+SELECT * FROM Student
+UNION ALL
+SELECT * FROM StudentNew;
